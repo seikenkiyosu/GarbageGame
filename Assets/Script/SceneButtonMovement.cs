f917d05a8c07d871de86824b904d8eb5a9357958
seikenkiyosu <seikenkiyosu@icloud.com>
2016-11-29 04:45:35 +0900

    StartMenu Scene making now

---
+++ Assets/Script/SceneButtonMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneButtonMovement : MonoBehaviour {
+
+    public GameObject SceneButtons;
+
+    void Awake () {
+        SceneButtons.SetActive(true);
+    }
+
+
+
+    /* OnClickButton */
+    public void OnClickUpButton () {
+        Debug.Log("Up button!!");
+        SceneButtons.transform.position = new Vector3 (
+            SceneButtons.transform.position.x,
+            SceneButtons.transform.position.y + 1000,
+            SceneButtons.transform.position.z
+            );
+    }
+
+    public void OnClickRightButton()
+    {
+        Debug.Log("Right button!!");
+        SceneButtons.transform.position = new Vector3(
+            SceneButtons.transform.position.x - 1000,
+            SceneButtons.transform.position.y,
+            SceneButtons.transform.position.z
+            );
+    }
+
+    public void OnClickLeftButton()
+    {
+        SceneButtons.transform.position = new Vector3(
+            SceneButtons.transform.position.x + 1000,
+            SceneButtons.transform.position.y,
+            SceneButtons.transform.position.z
+            );
+    }
+
+    public void OnClickDownButton()
+    {
+        SceneButtons.transform.position = new Vector3(
+            SceneButtons.transform.position.x,
+            SceneButtons.transform.position.y - 1000,
+            SceneButtons.transform.position.z
+            );
+    }
+}
